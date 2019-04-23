04a6fb62f5c0069dcde9767aafd934f09d5431c7
jianhai.wang <jianhai.wang@17zuoye.com>
2019-04-23 19:42:37 +0800

    fix socket port

--- gunicorn_dev_conf.py
+++ gunicorn_dev_conf.py
@@ -3,7 +3,7 @@ monkey.patch_all()
 import multiprocessing
 debug = True
 loglevel = 'debug'
-bind = '127.0.0.1:8000'
+bind = '127.0.0.1:8080'
 pidfile = '/tmp/gunicorn.pid'
 logfile = '/tmp/gunicorn.log'
 workers = multiprocessing.cpu_count() * 2 + 1
--- start_gunic_dev.sh
+++ start_gunic_dev.sh
@@ -1,1 +1,1 @@
-gunicorn -c /Users/ahaii/Python/blog/gunicorn_dev_conf.py -b 127.0.0.1:8080 blog.wsgi:application>>/tmp/gunicron.log 2>&1 &
+gunicorn -c /Users/ahaii/Python/blog/gunicorn_dev_conf.py blog.wsgi:application>>/tmp/gunicorn.log 2>&1 &
